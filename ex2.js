ac50f635ef4704f79684f4c2b21c5f4970a394c7
your-m1nd <vi.neurogirl@gmail.com>
2023-05-11 22:39:26 +0300

    change html, styles.css and js for ex.2

--- ex2.js
+++ ex2.js
@@ -1,17 +1,15 @@
-function showImg1 ()
-{
-    let Img2_holder = document.getElementById('Img2_holder');
-    let Img2 = document.getElementById('Img2');
-    Img2.src = '/assets/img_cat1.jpg';
+function showImg1(){
+    let Img1 = document.getElementById('Img1');
+    Img1.src = '/assets/img_cat1.jpg';
 };
 
-function showImg2 ()
-{
-    let Img1_holder = document.getElementById('Img1_holder');
+function showImg2(){
     let Img1 = document.getElementById('Img1');
     Img1.src = '/assets/img_cat2.jpg';
 };
 
+showImg2();
+showImg1();
 
 
 
